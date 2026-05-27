35795e266a8e624810119f514750e801949bf07e
nia-pencheva <petralewis.art@gmail.com>
2026-05-27 09:59:06 +0300

    Auth: Added admin guard and renamed requiresAuth to just auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: () => import("@/views/HomeView.vue"),
     meta: {
-      requiresAuth: true
+      auth: true
     }
   }
 ];
@@ -28,7 +28,8 @@ const router = createRouter({
 router.beforeEach(function(to: RouteLocationNormalizedGeneric, from: any, next: any) {
   const auth = useAuth();
 
-  if(to.meta.requiresAuth && !auth.isAuthenticated) next("/login");
+  if(to.meta.auth && !auth.isAuthenticated) next("/login");
+  else if(to.meta.admin && !auth.user?.isAdmin()) return { name: 'Forbidden' };
   else if(to.meta.guest && auth.isAuthenticated) next("/");
   else next();
 });
