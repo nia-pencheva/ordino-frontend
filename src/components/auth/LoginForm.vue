3f4d72f8b5b8fe687d81d5d53db032facfe16292
nia-pencheva <petralewis.art@gmail.com>
2026-05-27 10:16:08 +0300

    Auth: Finished the login form

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,40 +1,65 @@
 <template>
-    <form @submit.prevent="handleSubmit()">
-        <h3>Login</h3>
+    <TheWindow style="width: 100%; max-width: 400px;">
+        <div class="login-form">
+            <h2>Login</h2>
 
-        <div>
-            <label>Username</label>
-            <input 
-                v-model="username"
-                type="username" 
-                placeholder="Username" 
-            />
+            <div class="login-form__fields">
+                <div class="login-form__field__wrapper">
+                    <label for="username">Username</label>
+                    <TextInput 
+                        v-model="username"
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                    />
+                </div>
+
+                <div class="login-form__field__wrapper">
+                    <label for="password">Password</label>
+                    <TextInput 
+                        v-model="password"
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                    />
+                </div>
+            </div>
+
+            <p 
+                v-if="hasLoginError"
+                class="login-form__wrong-credentials"    
+            >
+                {{ loginError }}
+            </p>
+
+            <TheButton 
+                @click="handleLogin"
+                class="login-form__button"
+            >
+                Login
+            </TheButton>
         </div>
-
-        <div>
-            <label>Password</label>
-            <input 
-                v-model="password"
-                type="password" 
-                placeholder="Password" 
-            />
-        </div>
-
-        <button>Login</button>
-    </form>
-
-    <div v-if="hasLoginError">
-        {{ loginError }}
-    </div>
+    </TheWindow>
 </template>
 
 <script setup lang="ts">
+    // Imports
     import router from '@/router';
-    import { useAuth } from '@/store/auth';
-    import { computed, ref } from 'vue';
-    import { LoginRequest, LoginResponse } from './login-models';
-    import { fetchAPI } from '@/service/api/api';
+    import { useAuth } from '@/store/auth/auth';
+    import { computed, onMounted, onUnmounted, ref } from 'vue';
     import { UnauthorizedError } from '@/service/api/models/response-errors';
+
+    // Components
+    import TheWindow from '../base/TheWindow.vue';
+    import TheButton from '../base/TheButton.vue';
+    import TextInput from '../base/TextInput.vue';
+
+    // Vars
+    let enterEventListener = (event: KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            handleLogin();
+        }
+    };
 
     // Store
     const auth = useAuth();
@@ -48,19 +73,13 @@
     const hasLoginError = computed<boolean>(() => loginError.value != "");
 
     // Functions
-    async function handleSubmit() {
+    async function handleLogin() {
         try {
-            loginError.value = "";
-
-            const loginRequest: LoginRequest = {
-                username: username.value,
-                password: password.value
-            };
-
-            const response = await fetchAPI<LoginResponse>('login', 'POST', loginRequest);
-
-            auth.login(response.token);
-            router.replace('/');
+            auth.login({
+                    username: username.value,
+                    password: password.value
+                })
+                .then(() => router.replace('/'));
         }
         catch(error: any) {
             if(error instanceof UnauthorizedError) {
@@ -68,4 +87,46 @@
             }
         }
     }
+
+    // Lifecycle hooks
+    onMounted(() => {
+        document.addEventListener('keydown', enterEventListener);
+    });
+
+    onUnmounted(() => {
+        document.removeEventListener('keydown', enterEventListener);
+    });
 </script>
+
+<style lang="scss">
+    .login-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .login-form__fields {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+        width: 100%;
+        margin: 20px 0px;
+    }
+
+    .login-form__field__wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    .login-form__wrong-credentials {
+        margin: 0px 0px 20px;
+        color: $fontColorError;
+    }
+
+    .login-form__button {
+        width: 75px;
+        padding: 4px 18px;
+    }
+</style>
