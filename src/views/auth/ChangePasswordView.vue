cdb23b45447e59e7b426cfbbb23d87f0c54b58e3
nia-pencheva <petralewis.art@gmail.com>
2026-06-01 17:34:52 +0300

    Auth: Made login form use new base components; Added changing password on first login

---
+++ src/views/auth/ChangePasswordView.vue
@@ -0,0 +1,16 @@
+<template>
+    <AuthLayout>
+        <TheTitle>Change Password</TheTitle>
+
+        <ChangePasswordForm  />
+    </AuthLayout>
+</template>
+
+<script setup lang="ts">
+    // Components
+    import ChangePasswordForm from '@/components/auth/ChangePasswordForm.vue';
+    import AuthLayout from '@/components/layout/AuthLayout.vue';
+    import TheTitle from '@/components/layout/TheTitle.vue';
+</script>
+
+
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <AuthLayout>
+        <TheTitle>Login</TheTitle>
+
+        <LoginForm />
+    </AuthLayout>
+</template>
+
+<script setup lang="ts">
+    import TheTitle from '@/components/layout/TheTitle.vue';
+    import LoginForm from '@/components/auth/LoginForm.vue';
+    import AuthLayout from '@/components/layout/AuthLayout.vue';
+</script>
+
+
