9eb998ea0b0cca8364518fe33e75ba1c97b56924
nia-pencheva <petralewis.art@gmail.com>
2026-06-11 11:04:58 +0300

    Users: Moved user info from the general users list to a separate view; Added pagination, filtering and search

--- src/components/users/UsersTableRow.vue
+++ src/components/users/UsersTableRow.vue
@@ -1,20 +1,9 @@
 <template>
     <div class="users-table__row__wrapper">
         <div class="users-table__row"
-            @click="toggleDetails"  
-            :class="[ detailsOpen ? 'users-table__row--details-open' : 'users-table__row--details-closed']"
+            @click="router.push({ name: 'user-info', params: { id: user.id } })" 
         >
-            <div>
-                {{ props.user.fullName }}
-            </div>
-
-            <div>
-                <TheButton
-                    class="users-table__toggle-details"
-                >
-                    <i :class="[ detailsOpen ? 'users-table__toggle-details__icon--close' : 'users-table__toggle-details__icon--open' ]"></i>
-                </TheButton>
-            </div>
+            <p>{{ props.user.fullName }}</p>
         </div>
 
         <div 
@@ -22,68 +11,16 @@
             class="users-table__row__details__wrapper"
         >
             <div class=users-table__row__details>
-                <p>
-                    <b>
-                        Username
-                    </b>: 
-                    {{ props.user.username }}
-                </p>
-                <p>
-                    <b>
-                        Email
-                    </b>: 
-                    {{ props.user.email }}
-                </p>
-                <p>
-                    <b>
-                        Phone Number
-                    </b>: 
-                    {{ props.user.phoneNumber }}
-                </p>
-                <p style="text-transform: capitalize;">
-                    <b>
-                        Roles
-                    </b>: 
-                    {{ props.user.roles.join(", ") }}
-                </p>
-            </div>
-            
-            <div class="users-table__row__details__buttons">
-                <TheButton 
-                    class="users-table__row__details__button"
-                    @click="router.push('/users/' + props.user.id)"
-                >
-                    Edit
-                </TheButton>
-                <TheButton 
-                    class="users-table__row__details__button"
-                    style="width: 125px"
-                    @click="emit('open-reset-password-popup', props.user.id, props.user.username)"
-                >
-                    Reset password
-                </TheButton>
-                <TheButton 
-                    v-if="auth.user?.username != props.user.username"
-                    class="users-table__row__details__button"
-                    type="important"
-                    @click="emit('open-delete-popup', props.user.id, props.user.username)"    
-                >
-                    Delete
-                </TheButton>
+                
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import router from '@/router';
     import { ref } from 'vue';
-    import { User } from './users-models.js';
-    import { useAuth } from '@/store/auth/auth';
-
-    import TheButton from '../base/TheButton.vue';
-
-    const auth = useAuth();
+    import { User } from './users-models';
+    import router from '@/router/index';
 
     interface Props {
         user: User
@@ -96,11 +33,6 @@
     function toggleDetails() {
         detailsOpen.value = !detailsOpen.value;
     }
-
-    const emit = defineEmits<{
-        'open-reset-password-popup': [id: Number, username: string]
-        'open-delete-popup': [id: Number, username: string]
-    }>();
 </script>
 
 <style lang="scss">
@@ -115,54 +47,24 @@
             inset -1px 0 0 rgba(255, 255, 255, 0.55),
             0 1px 2px rgba(0, 0, 0, 0.20),
             0 1px 0 rgba(255, 255, 255, 0.60);
+
+        p {
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+        }
     }
 
     .users-table__row {
-        padding: 10px;
-        border-bottom: 1px solid $lightGrayBorder;
-        background: linear-gradient(#fff, #dcdcdc);
-    }
-
-    [class*="users-table__row--"] {
         cursor: pointer;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+        padding: 10px;
         background-color: white;
         border-radius: 5px;
-    }
-
-    .users-table__row--details-open {
-        border-radius: 5px 5px 0px 0px;
-    }
-
-    .users-table__row--details-closed {
-        border-radius: 5px;
-    }
-
-    .users-table__toggle-details {
-        right: 5px;
-        top: 10px;
-        width: 25px;
-        height: 25px;
-        font-size: 18px;
-    }
-
-    .users-table__toggle-details__icon--open {
-        @include arrow-down(black, 2px);
-
-        position: relative;
-        bottom: 2px;
-        right: 0px;
-    }
-
-    .users-table__toggle-details__icon--close {
-        @include arrow-up(black, 2px);
-
-        position: relative;
-        top: 1px;
-        left: 0px;
+        height: $tableRowHeight;
     }
 
     .users-table__row__details__wrapper {
@@ -179,18 +81,5 @@
         display: flex;
         flex-direction: column;
         gap: 10px;
-    }
-
-    .users-table__row__details__buttons {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 5px;
-        flex-wrap: wrap;
-    }
-
-    .users-table__row__details__button {
-        width: 75px;
-        padding: 4px;
     }
 </style>
---
+++ src/views/users/UserInfoView.vue
@@ -0,0 +1,137 @@
+<template>
+    <TheLayout>
+        <div
+            v-if="user == undefined"
+            class="user-info-view__loader"
+        >
+            <TheSpinner size="lg" />
+        </div>
+
+        <template v-else>
+            <TheTitle>{{ user.fullName }}</TheTitle>
+
+            <div class="user-info__acitons-toolbar">
+                <TheButton
+                    class="user-info__actions-toolbar__button"
+                    @click="router.push({ name: 'edit-user', params: { id: user.id } })"
+                >
+                    Edit
+                </TheButton>
+                <TheButton 
+                    class="user-info__actions-toolbar__button"
+                    style="width: 125px"
+                    @click="resetPasswordPopupOpen = true"
+                >
+                    Reset password
+                </TheButton>
+                <TheButton 
+                    v-if="auth.user?.username != user.username"
+                    class="user-info__actions-toolbar__button"
+                    type="important"
+                    @click="deletePopupOpen = true"    
+                >
+                    Delete
+                </TheButton>
+            </div>
+
+            <div>
+                <SectionTitle>Information</SectionTitle>
+
+                <p class="user-info__info-row">
+                    <b>Username</b>: 
+                    {{ user.username }}
+                </p>
+                <p class="user-info__info-row">
+                    <b>Email</b>: 
+                    {{ user.email }}
+                </p>
+                <p class="user-info__info-row">
+                    <b>Phone Number</b>: 
+                    {{ user.phoneNumber }}
+                </p>
+                <p 
+                    class="user-info__info-row" 
+                    style="text-transform: capitalize;"
+                >
+                    <b>Roles</b>: 
+                    {{ user.roles.join(", ") }}
+                </p>
+            </div>
+
+            <ResetPasswordPopup
+                v-if="resetPasswordPopupOpen"
+                :user-id="user.id"
+                :username="user.username"
+                @close="resetPasswordPopupOpen = false"
+            />
+
+            <DeleteUserPopup
+                v-if="deletePopupOpen"
+                :user-id="user.id"
+                :username="user.username"
+                @close="deletePopupOpen = false"
+                @deleted="router.back()"
+            />
+        </template>
+    </TheLayout>
+</template>
+
+<script setup lang="ts">
+    import { APICall } from '@/service/api/api';
+    import { onMounted, ref } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    import TheLayout from '@/components/layout/TheLayout.vue';
+import TheSpinner from '@/components/base/TheSpinner.vue';
+import TheTitle from '@/components/layout/TheTitle.vue';
+import TheButton from '@/components/base/TheButton.vue';
+import router from '@/router';
+import { User } from '@/components/users/users-models';
+import ResetPasswordPopup from '@/components/users/ResetPasswordPopup.vue';
+import DeleteUserPopup from '@/components/users/DeleteUserPopup.vue';
+import { useAuth } from '@/store/auth/auth';
+import SectionTitle from '@/components/base/SectionTitle.vue';
+
+    const route = useRoute();
+
+    const auth = useAuth();
+
+    const user = ref<User | undefined>(undefined);
+    const resetPasswordPopupOpen = ref<boolean>(false);
+    const deletePopupOpen = ref<boolean>(false);
+
+    async function fetchUser() {
+        user.value = await (new APICall<User>(`users/${route.params.id}`)).execute();
+    }
+
+    onMounted(async() => {
+        fetchUser();
+    })
+</script>
+
+<style lang="scss">
+    .user-info-view__loader {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .user-info__acitons-toolbar {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 5px;
+        flex-wrap: wrap;
+        margin-bottom: 20px;
+    }
+
+    .user-info__actions-toolbar__button {
+        width: 75px;
+        padding: 4px;
+    }
+
+    .user-info__info-row {
+        margin-bottom: 8px;
+    }
+</style>
